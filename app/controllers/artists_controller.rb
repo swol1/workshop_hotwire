967245299813a964788063fa8720195ff8bd7704
swol1 <den.gavrilin1@gmail.com>
2023-07-31 11:14:52 +0200

    Implement Load more functionality for popular tracks

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,16 +2,29 @@ class ArtistsController < ApplicationController
   def show
     artist = Artist.find(params[:id])
     albums = selected_albums(artist.albums, params[:album_type]).with_attached_cover.preload(:artist)
-    tracks = artist.tracks.popularity_ordered.limit(5)
+    limit, tracks, tracks_amount = fetch_popular_tracks_with_count(artist)
 
     if turbo_frame_request?
-      render partial: "discography", locals: {artist:, albums:}
+      if turbo_frame_request_id.match?(/discography/)
+        render partial: "discography", locals: {artist:, albums:}
+      elsif turbo_frame_request_id.match?(/popular_tracks/)
+        render partial: "popular_tracks", locals: {artist:, tracks:, limit:, tracks_amount:}
+      end
     else
-      render action: :show, locals: {artist:, albums:, tracks:}
+      render action: :show, locals: {artist:, albums:, tracks:, limit:, tracks_amount:}
     end
   end
 
   private
+
+  def fetch_popular_tracks_with_count(artist)
+    limit = [params.fetch(:limit, 0).to_i, 0].max + 5
+    tracks = artist.tracks.popularity_ordered
+    tracks_amount = tracks.count
+    tracks = tracks.limit(limit)
+  
+    [limit, tracks, tracks_amount]
+  end
 
   def selected_albums(albums, album_type)
     return albums.lp if album_type.blank?
